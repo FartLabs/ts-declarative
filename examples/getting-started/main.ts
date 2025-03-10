7161730b485750be2395b0487e2aae12a6da0edc
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-03-09 17:37:19 -0700

    Create function declarativeTsMorph

--- examples/getting-started/main.ts
+++ examples/getting-started/main.ts
@@ -1,15 +1,14 @@
 import { createDecoratorFactory } from "#/lib/declarative/decorator.ts";
 import { DeclarativeStorageInMemory } from "#/lib/declarative/storage/in-memory.ts";
 import { getClassID } from "#/lib/declarative/declarative.ts";
-import type { Context } from "#/examples/context/context.ts";
+import type { Context, StateContext } from "#/examples/context/context.ts";
 import { declarativeContext } from "#/examples/context/context.ts";
-import type { DocNodeClass } from "#/examples/deno-doc/deno-doc.ts";
+import type { StateDenoDoc } from "#/examples/deno-doc/deno-doc.ts";
 import { declarativeDenoDoc } from "#/examples/deno-doc/deno-doc.ts";
+import type { StateTsMorph } from "#/examples/ts-morph/ts-morph.ts";
+import { declarativeTsMorph } from "#/examples/ts-morph/ts-morph.ts";
 
-export interface State {
-  context?: Context;
-  denoDoc?: DocNodeClass;
-}
+export interface State extends StateContext, StateDenoDoc, StateTsMorph {}
 
 export const storage = new DeclarativeStorageInMemory<State>();
 export const prefix = `${import.meta.url}#`;
@@ -19,10 +18,11 @@ export const context = createDecoratorFactory(
     prefix,
     initialize: (context?: Context): State => ({ context }),
   },
-  await declarativeDenoDoc(import.meta.url.toString(), {
+  await declarativeDenoDoc(import.meta.url, {
     importMap: new URL("../../deno.json", import.meta.url).toString(),
     includeAll: true,
   }),
+  await declarativeTsMorph(new URL(import.meta.url)),
   declarativeContext(prefix),
 );
 
@@ -35,8 +35,6 @@ export class Person {
 
 if (import.meta.main) {
   const classID = getClassID(Person);
-
-  // deno -A examples/getting-started/main.ts
   console.log(classID, storage.get(classID!));
 
   // file:///C:/Users/ethan/Documents/GitHub/ts-declarative/examples/getting-started/main.ts#Person {
@@ -51,30 +49,46 @@ if (import.meta.main) {
   //       filename: "file:///C:/Users/ethan/Documents/GitHub/ts-declarative/examples/getting-started/main.ts",
   //       line: 32,
   //       col: 0,
-  //       byteIndex: 1055
+  //       byteIndex: 1253
   //     },
-  //     declarationKind: "export",
-  //     kind: "class",
-  //     classDef: {
-  //       isAbstract: false,
-  //       constructors: [
-  //         {
-  //           accessibility: "public",
-  //           hasBody: true,
-  //           name: "constructor",
-  //           params: [Array],
-  //           location: [Object]
-  //         }
-  //       ],
-  //       properties: [],
-  //       indexSignatures: [],
-  //       methods: [],
-  //       extends: undefined,
-  //       implements: [],
-  //       typeParams: [],
-  //       superTypeParams: [],
-  //       decorators: [ { name: "context", args: [Array], location: [Object] } ]
-  //     }
+  //     ...
+  //   },
+  //   tsMorph: {
+  //     decorators: [
+  //       {
+  //         kind: 7,
+  //         name: "context",
+  //         arguments: [ '{\n  name: "https://schema.org/name",\n}' ],
+  //         typeArguments: []
+  //       }
+  //     ],
+  //     typeParameters: [],
+  //     docs: [],
+  //     isAbstract: false,
+  //     implements: [],
+  //     name: "Person",
+  //     isExported: true,
+  //     isDefaultExport: false,
+  //     hasDeclareKeyword: false,
+  //     kind: 2,
+  //     ctors: [
+  //       {
+  //         statements: [],
+  //         parameters: [ [Object] ],
+  //         returnType: undefined,
+  //         typeParameters: [],
+  //         docs: [],
+  //         scope: "public",
+  //         kind: 5,
+  //         overloads: []
+  //       }
+  //     ],
+  //     staticBlocks: [],
+  //     methods: [],
+  //     properties: [],
+  //     extends: undefined,
+  //     getAccessors: [],
+  //     setAccessors: []
   //   }
   // }
 }

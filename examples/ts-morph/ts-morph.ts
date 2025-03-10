8a4bb2d87235f355d7ec1a4f1e222928c00391b6
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-03-09 19:15:32 -0700

    Add property types

--- examples/ts-morph/ts-morph.ts
+++ examples/ts-morph/ts-morph.ts
@@ -6,7 +6,13 @@ import type { Declarative } from "#/lib/declarative/declarative.ts";
  * TsMorph is a declarative class type from ts-morph.
  */
 export interface TsMorph {
-  properties: Array<[string, string]>;
+  properties: TsMorphProperty[];
+}
+
+export interface TsMorphProperty {
+  name: string;
+  type: string;
+  paramIndex?: number;
 }
 
 export interface StateTsMorph {
@@ -31,14 +37,30 @@ export async function declarativeTsMorph<TState extends StateTsMorph>(
 
 function getTsMorph(sourceFile: SourceFile, name: string): TsMorph {
   const classDeclaration = sourceFile.getClass(name);
-  const propertyDeclarations = classDeclaration?.getProperties();
-  if (propertyDeclarations === undefined) {
-    throw new Error(`Could not find property declarations for ${name}`);
-  }
+  const propertyDeclarations = classDeclaration?.getProperties() ?? [];
+  const constructorParameterDeclarations = classDeclaration
+    ?.getConstructors()
+    .at(-1)
+    ?.getParameters()
+    ?.filter((param) => param.getScope() === "public") ?? [];
 
   return {
-    properties: propertyDeclarations.map((property) => {
-      return [property.getName(), property.getType().getText()];
-    }),
+    properties: [
+      ...propertyDeclarations.map((property): TsMorphProperty => {
+        return {
+          name: property.getName(),
+          type: property.getType().getText(),
+        };
+      }),
+      ...constructorParameterDeclarations.map(
+        (parameter, i): TsMorphProperty => {
+          return {
+            name: parameter.getName(),
+            type: parameter.getType().getText(),
+            paramIndex: i,
+          };
+        },
+      ),
+    ],
   };
 }

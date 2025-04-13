e02b14020b667911a18d22e10ba7a41e2aa679aa
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-04-13 16:57:27 -0700

    Create function createOazapftsClientOf

--- lib/declarative/common/openapi/oazapfts/e2e.test.ts
+++ lib/declarative/common/openapi/oazapfts/e2e.test.ts
@@ -1,10 +1,9 @@
-import { assertEquals } from "@std/assert/equals";
+import { assert } from "@std/assert";
 import { assertSnapshot } from "@std/testing/snapshot";
-import { openapi } from "#/lib/declarative/common/openapi/server.ts";
+import { openapiSpec } from "#/lib/declarative/common/openapi/specification.ts";
 import { standardMethodsWithDenoKv } from "#/lib/declarative/common/google-aip/deno-kv.ts";
 import { createAutoSchemaDecoratorFactoryAt } from "#/lib/declarative/common/json-schema/auto-schema/auto-schema.ts";
-import { generateOazapftsClientOf } from "./client.ts";
-import { routerOf } from "#/lib/declarative/common/router/router.ts";
+import { createOazapftsClientOf, generateOazapftsClientOf } from "./client.ts";
 
 const autoSchema = await createAutoSchemaDecoratorFactoryAt(import.meta);
 
@@ -18,41 +17,21 @@ class Person {
   public constructor(public name: string) {}
 }
 
-@openapi({
-  specification: { servers: [{ url: "http://localhost:8080" }] },
-  resources: [Person],
-})
+@openapiSpec({ resources: [Person] })
 class App {}
 
 Deno.test(
   "generateOazapftsClientOf generates client from OpenAPI specification",
   async (t) => {
-    const sourceCode = await generateOazapftsClientOf(
-      App,
-      { optimistic: true },
-    );
+    const sourceCode = await generateOazapftsClientOf(App, {
+      optimistic: true,
+    });
     await assertSnapshot(t, sourceCode);
   },
 );
 
-Deno.test("e2e Oazapfts client dynamic import", async () => {
-  const server = Deno.serve({ port: 8080 }, routerOf(App));
-  const ash = new Person("Ash Ketchum");
-
-  const sourceCode = await generateOazapftsClientOf(App, { optimistic: true });
-  if (sourceCode === undefined) {
-    throw new Error("sourceCode is undefined");
-  }
-
-  const client = await import(
-    `data:text/typescript;base64,${btoa(sourceCode)}`
-  );
-
-  const createdPerson = await client.postPeople(ash);
-  assertEquals(createdPerson.name, ash.name);
-
-  const fetchedPerson = await client.getPeopleByPerson(ash.name);
-  assertEquals(fetchedPerson.name, ash.name);
-
-  await server.shutdown();
+Deno.test("generateOazapftsClientOf dynamic import", async (_t) => {
+  const client = await createOazapftsClientOf(App);
+  assert(Object.hasOwn(client, "getPeopleByPerson"));
+  assert(Object.hasOwn(client, "postPeople"));
 });

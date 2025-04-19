58ee67a4aee40e2997514d780afeff707e16f82a
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-04-18 22:28:03 -0700

    Update custom-methods.ts

--- lib/declarative/common/google-aip/custom-methods/custom-methods.ts
+++ lib/declarative/common/google-aip/custom-methods/custom-methods.ts
@@ -12,6 +12,8 @@ import {
   toOperationSchema,
 } from "#/lib/declarative/common/google-aip/operation.ts";
 
+export type { Handler };
+
 /**
  * customMethod is a decorator factory that creates a custom method for the
  * resource.
